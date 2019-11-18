d717c1e6b56b47076b6c504247bf1c63c82561dd
Fournier Allan <fournier@b11p14.fil.univ-lille1.fr>
2019-11-18 10:35:18 +0100

    Fin TP

---
+++ src/main/java/com/ifi/trainer_api/repository/TrainerRepository.java
@@ -0,0 +1,12 @@
+package com.ifi.trainer_api.repository;
+
+import com.ifi.trainer_api.bo.Trainer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TrainerRepository extends CrudRepository<Trainer, String> {
+
+}
---
+++ src/main/java/com/ifi/trainer_api/service/TrainerServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ifi.trainer_api.service;
+
+import com.ifi.trainer_api.bo.Trainer;
+import com.ifi.trainer_api.repository.TrainerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TrainerServiceImpl implements TrainerService{
+    private TrainerRepository trainerRepository;
+
+    public TrainerServiceImpl(TrainerRepository trainerRepository) {
+        this.trainerRepository = trainerRepository;
+    }
+
+    @Override
+    public Iterable<Trainer> getAllTrainers() {
+        // TODO
+        return trainerRepository.findAll();
+    }
+
+    @Override
+    public Trainer getTrainer(String name) {
+        // TODO
+        return trainerRepository.findById(name).orElse(null);
+    }
+
+    @Override
+    public Trainer createTrainer(Trainer trainer) {
+        // TODO
+        return  trainerRepository.save(trainer);
+    }
+}
